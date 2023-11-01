ec9877533a35c1a6203ee121a2d312b705987f4d
John Doe <johndoe@example.com>
2023-11-01 20:18:20 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,19 +1,18 @@
-import {createFormReservation} from './modules/createFormReservation.js';
-
 const init = async () => {
   const dataModule = await import('./modules/apiTravel.js');
-  const dateFormModule = await import('./modules/createFormDateSelection.js');
+  const getDateFormModule = await import('./modules/createFormDateSelection.js');
   const timerModule = await import('./modules/creareTimerMain.js');
   const data = await dataModule.getDate();
   const timer = timerModule.createTimerMain();
   const heroTextElement = document.querySelector('.hero__text');
   const tourContent = document.querySelector('.tour__content');
-  const dateForm = dateFormModule.createFormDateSelection(data);
+  const getDateForm = getDateFormModule.createFormDateSelection(data);
   const reservationContainer = document.querySelector('.reservation__container');
-  const formReservation = createFormReservation(data);
+  const getFormReservationModule = await import('./modules/createFormReservation.js');
+  const getFormReservation = getFormReservationModule.createFormReservation(data);
 
-  reservationContainer.append(formReservation);
-  tourContent.append(dateForm);
+  reservationContainer.append(getFormReservation);
+  tourContent.append(getDateForm);
 
   heroTextElement.insertAdjacentElement('afterend', timer);
 };
